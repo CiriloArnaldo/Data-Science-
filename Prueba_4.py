22c26b7b6bdeb3df518830912270427bc79188da
Chiri <cpalominou@uni.pe>
2018-12-17 20:51:04 -0500

    Creando archivo CSV

--- Prueba_4.py
+++ Prueba_4.py
@@ -1,12 +1,12 @@
 import tweepy
 from textblob import TextBlob
+import pandas as pd
 
 # Step 1 - Authenticate
-consumer_key= 'gaotNxAHDnrHvTgVVjlddzbgp'
-consumer_secret= 'QauM5qAMOyZ8326XzuRcNbbdjHZrJRIGKbiIqzZ6QwdenJisjJ'
-
-access_token='1072612111568777223-K5OdWKvBczN15nDhSUTdmPw51IwNMo'
-access_token_secret='RX6aC2o8nalqTCNy0IvbuaBJDJ9PiS1vZrkzcsJ4NVejE'
+consumer_key = '6GA9Cq6EcMazscYbtfwZ0fKjz'
+consumer_secret = '9jKmzEpyCCcrPaTu6NHSV03nNo03pUplfLiN6VFskes9hJOxkX'
+access_token = '1072612111568777223-jeK0cG4sXJy8mNEm1QhGUK4Hhrb8UU'
+access_token_secret = '9lFGtCgrc5PDosyq3g3Cyimwtowg7xzIgVWnLlD4oQ5Y1'
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
@@ -22,11 +22,21 @@ public_tweets = api.search('Trump')
 #and label each one as either 'positive' or 'negative', depending on the sentiment
 #You can decide the sentiment polarity threshold yourself
 
+df = pd.DataFrame(columns=['Text','Sentiment_level'])
+i = 0
 
 for tweet in public_tweets:
     print(tweet.text)
-
+    print(type(tweet.text))
     #Step 4 Perform Sentiment Analysis on Tweets
     analysis = TextBlob(tweet.text)
     print(analysis.sentiment)
+    print(type(analysis.sentiment.polarity))
     print("")
+
+    df.loc[i] = [tweet.text , round(analysis.sentiment.polarity,2)]
+    i += 1
+
+print(df)
+
+df.to_csv('Datos.csv')
